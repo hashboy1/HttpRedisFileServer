c74a7295a79dc8b693c35eacc814dd45febd5785
hashboy1 <angel_ghost1121@163.com>
2018-06-14 13:19:53 +0800

    version 1

---
+++ src/main/java/com/NioHttpFileServer/configer.java
@@ -0,0 +1,7 @@
+package com.NioHttpFileServer;
+
+public class configer {
+      public static String RedisIp = "192.168.0.160";
+      public static int RedisPort = 6379;
+      public static String encoding = "ISO-8859-1";
+}
